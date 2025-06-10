a49695ca437048a0e279b61cdbd9ca116be9b28d
Marcelo <marcelo.gs2002@gmail.com>
2025-06-10 16:55:35 -0300

    Updated flutter pub and connect to supabase

--- contata_aqui/lib/main.dart
+++ contata_aqui/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://zzvdnfwamqbbjwbsfjap.supabase.co',
+    anonKey: '7pjUkqYqMa*!4?u',                
+  );
+
   runApp(App());
 }
